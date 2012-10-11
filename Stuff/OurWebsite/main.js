c5423ad8956cc05c17397fc6f0bece57c6292f2c
ryanschuhler <ryan.schuhler@liferay.com>
2012-10-11 16:49:32 -0700

    first commit

---
+++ Stuff/OurWebsite/main.js
@@ -0,0 +1,45 @@
+AUI().use('transition').ready(
+	function (A) {
+		// var homepage = A.one('#homepage');
+		// var navigation = A.one('#navigation');
+		// var navItem = navigation.all('a');
+		// var navItemHome = navigation.all('li');
+		// var tranOpacity = function (e, params1, params2) {
+		// 	e.on(
+		// 		'hover',
+		// 		function (event) {
+		// 			event.currentTarget.transition(params1)
+		// 		},
+
+		// 		function (event) {
+		// 			if (homepage) {
+		// 				event.currentTarget.transition(params2);
+		// 			} else if (!event.currentTarget.ancestor('li').hasClass('selected') ) {
+		// 				event.currentTarget.transition(params2);
+		// 			}
+		// 		}
+		// 	);
+		// };
+
+		// var pOne = {
+		// 	easing: 'ease-out',
+		// 	duration: 1.5,
+		// 	background: 'rgba(197, 231, 255, 1)'
+		// };
+
+		// var pTwo = {
+		// 	easing: 'ease-out',
+		// 	duration: 1.5,
+		// 	background: 'rgba(197, 231, 255, 0)'
+		// };
+
+		// if (homepage) {
+		// 	tranOpacity(navItemHome, pOne, pTwo);
+		// } else {
+		// 	pOne.background = '#C5E7FF';
+		// 	pTwo.background = '#0B364B';
+
+		// 	tranOpacity(navItem, pOne, pTwo);
+		// }
+	} 
+);
